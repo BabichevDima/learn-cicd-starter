ea525f0bb568bea821e906185cb078ded74dc5a2
Dmitry <dzmitry.babichev@gmail.com>
2025-07-22 13:17:46 +0300

    fix

--- json.go
+++ json.go
@@ -30,8 +30,7 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	_, err := w.Write(dat)
-	if err != nil {
+	if _, err := w.Write(dat); err != nil {
 		log.Printf("Failed to write response: %v", err)
 	}
 }
